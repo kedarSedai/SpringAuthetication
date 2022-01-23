975f5420f89586bfa9734a6bb1d9f6f3f8093663
kedar.sedai <kedar.sedai05@gmail.com>
2022-01-23 07:43:26 +0545

    first commit

---
+++ src/main/java/com/example/springauth/model/User.java
@@ -0,0 +1,34 @@
+package com.example.springauth.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+
+@Table(name = "training1")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 45)
+    private String email;
+
+    @Column(nullable = false, length = 64)
+    private String password;
+
+    @Column(name = "first_name", nullable = false, length = 20)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 20)
+    private String lastName;
+
+
+}
